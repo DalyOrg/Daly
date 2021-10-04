6bf1474274000685d93ff6899f02e894cae9f5db
auraBan06 <emre.ban@stonybrook.edu>
2021-10-04 01:03:07 -0400

    checking push

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Quizzes
+        welcome to daly quizzes
         <li>
         {quizzes ? quizzes.map((question) => {
           return <ul>{quizzes[0]}</ul>
