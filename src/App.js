0401373dc09f42db000eb9fea5ed03a13ccb1fe6
Faraz Fazli <farazfazli@gmail.com>
2021-10-27 23:50:49 -0400

    Update code and add signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics"; // TODO: add later perhaps?
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, 
     Route, 
     Switch, 
@@ -14,6 +17,23 @@ import LandingPage from "./webpages/LandingPage";
 import LoginPage from "./webpages/Login";
 import HomePage from "./webpages/HomePage";
 import TopBar from "./components/TopBar";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
+
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+
 
 
 
--- src/webpages/Login.jsx
+++ src/webpages/Login.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
+const signInWithGoogle = () => {
+// Adapted from documentation
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    // Send to DB
+  }).catch((error) => {
+    console.log(error.message);
+  });
+}
 
 const LoginPage = () => {
     return (
         <div>
-            Login
+            <button className="signinButton" onClick={signInWithGoogle}>Sign in with Google</button>
         </div>
     );
 }
