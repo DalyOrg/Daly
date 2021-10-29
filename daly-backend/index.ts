c8ecfff72c16b51be273e0374ed942a717f855d2
Faraz Fazli <farazfazli@gmail.com>
2021-10-29 19:54:07 -0400

    Update code

--- daly-backend/index.ts
+++ daly-backend/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import { toHttp } from './common/toHttp';
 import { CreateQuiz, GetQuiz, HelloWorld } from './controllers/QuizController';
+import passport from 'passport';
 
 const app = express();
 const port = 8080;
@@ -14,6 +15,13 @@ app.get('/', toHttp(HelloWorld));
 
 app.get('/quiz/:quizId', toHttp(GetQuiz));
 app.post('/quiz', toHttp(CreateQuiz));
+app.post('login', passport.authenticate('google'),
+(req: any, res: any) => {
+  if (req.user) {
+    console.log(req.user.username);
+    res.redirect('/users/'+req.user.username);
+  }
+});
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
