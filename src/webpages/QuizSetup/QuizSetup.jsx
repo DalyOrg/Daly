53ffe73016ba6216b7d4a342fc3182af8bc4d673
Qiting Wu <72841391+qitingwu@users.noreply.github.com>
2021-11-03 01:52:08 -0400

    minor changes to quiz setup page
    Changed style of image upload button
    
    Added character limit label
    
    Restrict only number input for time.

--- src/webpages/QuizSetup/QuizSetup.jsx
+++ src/webpages/QuizSetup/QuizSetup.jsx
@@ -1,48 +1,93 @@
-import React from "react";
 import "./quizSetup.css";
 import { MDBBtn } from 'mdb-react-ui-kit';
+import { useState } from "react";
 
-function displayCategoryTags(){
-    return ``;
-}
 
 const QuizSetup = () => {
+
+    //TODO: there can only be 10 tags for a quiz
+    const [categories, setCategories] = useState([]);
+
+    const [name, setName] = useState('');
+    const [time, setTime] = useState();
+    const [category, setCategory] = useState('');
+    //TODO: set background to new quiz summary page. 
+    //TODO: set background image object to URL?
+    const [background, setBackground] = useState();
+
+    function displayCategoryTags(){
+        return ``;
+    }
+
+    function updateBackground(event){
+        setBackground(event.target.files[0]);
+        return '';
+    }
+
+    function categoryStyle(){
+        if(category && category.length > 20)
+        {
+            return 'red';
+        }
+        else{
+            return 'white';
+        }
+            
+    }
+
     return (
         <div>
             <form id="quiz-setup">
                 <p class="quiz-setup-title"><b>Quiz Setup</b></p>
                 <div class="form-inputs">
 
-                    <label for="quizName">Quiz name:</label><br/>
-                    <input type="text" id="quizName" name="quizName" required></input><br/>
+                    <label class="reg-label" for="quizName">Quiz name:</label><br/>
+                    <input type="text" id="quizName" name="quizName" value={name ? name : ''} required 
+                    onChange={
+                        e=>setName(e.target.value)
+                    }
+                    ></input><br/>
                         
                     <br/>
 
-                    <label for="time">Time allotted (in minutes):</label><br/>
-                    <input type="text" id="time" name="time" required></input><br/>
-    
+                    <label class="reg-label" for="time">Time allotted (in minutes):</label><br/>
+                    <input type="text" id="time" name="time" value={time? time : ''} required 
+                    onChange={
+                        e=>setTime(e.target.value.replace(/\D/,''))
+                    }
+                    ></input><br/>
                     <br/>
 
-                    <form id="category-input">
-                        <label for="categories">Categories:</label><br/>
-                        <input class="categories" type="text" id="categories" name="categories"></input>
-                        <MDBBtn style={{color: "white", backgroundColor: "#CB12CB", marginLeft: '1rem', marginBottom: '1rem'}} rounded>+</MDBBtn>
-                    </form><br/>
+                    <label for="categories" style={{color: categoryStyle()}}>Categories: (Character limit: {category.length}/20)</label><br/>
+                    <input class="categories" type="text" id="categories" name="categories" value={category? category : ''}
+                    onChange={
+                        e=>setCategory(e.target.value)
+                    }
+                    ></input>
+                    <MDBBtn style={{color: "white", backgroundColor: "#CB12CB", marginLeft: '1rem', marginBottom: '1rem'}} rounded>+</MDBBtn>
+                    <br/>
                     {displayCategoryTags()}
     
                     <br/>
 
-                    <table>
+                    <table class="background">
                         <tr>
                             <th><h2 class="add-background">Add Background</h2></th>
-                            <th><MDBBtn style={{color: "white", backgroundColor: "#00B5FF", marginLeft: '1rem', marginBottom: '1rem'}} rounded>Upload Image</MDBBtn></th>
+                            <th><label class="reg-label" class="upload-button">
+                                <input type="file" name="backgroundImage" accept=".jpg,.png,.img" onChange={e=>updateBackground(e)}></input>
+                                Upload Image</label>
+                            </th>
                         </tr>
                     </table>
                     
                     <br/><br/>
                     
-
-                        <MDBBtn class="btn btn-primary" style={{color: "white", backgroundColor: "#00B5FF", position: "relative", left: "28%"}} rounded>Publish</MDBBtn>
+                        {/*TODO: uploads initial quiz data and redirects to quiz edit page when clicked*/}
+                        <MDBBtn class="btn btn-primary" style={{color: "white", backgroundColor: "#00B5FF", position: "relative", left: "28%"}} rounded 
+                            onClick={
+                                console.log(name, time, category, categories, background)
+                            }
+                        >Publish</MDBBtn>
                     </div>
             </form>
         </div>
