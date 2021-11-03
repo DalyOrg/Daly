1953e9a004f3f391c37dab7c32b5cc8d2d846d76
rchen10 <ricky.chen.2@stonybrook.edu>
2021-11-03 18:40:05 -0400

    Platform interface

---
+++ daly-backend/interfaces/platform.ts
@@ -0,0 +1,10 @@
+export interface Platform{
+    ownerId: string
+    name: string
+    quizzes: string[]
+    platformPicture: string
+    platformBanner: string
+    subscribersId: string
+    subscriberCount: number
+    chatId: string
+}
