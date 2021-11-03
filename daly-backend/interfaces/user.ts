9ba4cc8c1f991b489118b58162aa2c3756b3817b
rchen10 <ricky.chen.2@stonybrook.edu>
2021-11-03 18:42:33 -0400

    User interface

---
+++ daly-backend/interfaces/user.ts
@@ -0,0 +1,19 @@
+import { Timestamp } from "firebase-admin/firestore";
+
+export interface User{
+    id: string
+    accountCreated: Timestamp
+    username: string
+    badges: number
+    interestsId: string
+    platformsOwned: string[]
+    subscriptionsId: string
+    subscriptionFeedId: string
+    likesId: string
+    profilePicture: string
+    profileBanner: string
+    rewardsUnlocked: {
+        rewardId: string
+        level: number
+    }[]
+}
