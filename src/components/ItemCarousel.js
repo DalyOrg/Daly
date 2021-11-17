224cb467c439b96940fc7b5675a2ce1b9ff6a173
auraBan06 <emre.ban@stonybrook.edu>
2021-11-17 03:45:53 -0500

    improved carousels

--- src/components/ItemCarousel.js
+++ src/components/ItemCarousel.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-export default styled.button`
+export default 
+
+styled.button`
   display: flex;
   justify-content: flex-start;
   align-items: flex-end;
@@ -8,5 +10,18 @@ export default styled.button`
   width: 100%;
   margin: 0 15px;
   font-size: 3em;
-  border-radius: 25px;
-`;
+  border-radius: 5px;
+  opacity:0.85;
+
+  
+  &:hover {
+    -webkit-transform: scale(1.2);
+    -ms-transform: scale(1.2);
+    transform: scale(1.2);
+    opacity:1;
+  }
+`
+
+;
+
+
