77b820408e1e817ddbc11460c479cb264cca054c
Faraz Fazli <farazfazli@gmail.com>
2021-11-21 18:30:39 -0500

    Add search

--- src/webpages/SearchPage.jsx
+++ src/webpages/SearchPage.jsx
@@ -2,6 +2,7 @@ import { Funnel } from 'react-bootstrap-icons';
 import { useParams } from 'react-router';
 import { postSearch } from "../adapters/search";
 import { getQuiz } from "../adapters/quiz";
+import { getPlatform } from '../adapters/platform';
 import { useEffect, useState } from "react"
 
 const SearchPage = () => {
@@ -9,17 +10,24 @@ const SearchPage = () => {
     const [results, setResults] = useState("");
 
     useEffect(() => {
-        let quizzes = []
+        let tempResults = []
         postSearch(searchTerm).then((res) => {
             res.results.forEach((elem) => {
-              console.log("ID",elem.id);
-              getQuiz(elem.id).then((quizInfo) => {
-                quizzes.push(quizInfo);
-              })
+              if (elem.type === 'quiz') {
+                getQuiz(elem.id).then((quizInfo) => {
+                  console.log("Quiz Info", quizInfo);
+                  tempResults.push(quizInfo.data);
+                })
+              } else {
+                getPlatform(elem.id).then((platformInfo) => {
+                  console.log("Platform Info", platformInfo);
+                  tempResults.push(platformInfo.data);
+                });
+              }
             })
         })
-        setResults(quizzes);
-      }, [searchTerm])
+        setResults(tempResults);
+      }, [])
 
     return (
       <>
@@ -29,27 +37,19 @@ const SearchPage = () => {
         <Funnel style={{color: '#00B5FF', fontSize: '4rem'}}></Funnel>
         </div>
 
-        <div className='row border-bottom px-3 pb-4 mb-3'>
+        {results ? results.map((result) => {
+          <div className='row border-bottom px-3 pb-4 mb-3'>
             <div className="col-2 offset-1"><img src="http://placehold.it/200x100"></img></div>
             <div className='col-4'>
-            <h1 style={{color: 'white'}}>Ragdoll Quiz</h1>
+            <h1 style={{color: 'white'}}>{result['name']}</h1>
             <h3 style={{color: 'hotpink'}}>Created by Faraz Fazli</h3>
             </div>
             <div className='col-5' style={{color: 'hotpink'}}>
             <p>Available since: 9/15/21</p>
             </div>
         </div>
+        }) : "No results found"}
       </div>
-        <div className='row border-bottom px-3 pb-4'>
-            <div className="col-2 offset-1"><img src="http://placehold.it/200x100"></img></div>
-            <div className='col-4'>
-            <h1 style={{color: 'white'}}>Ragdoll Quiz</h1>
-            <h3 style={{color: 'hotpink'}}>Created by Faraz Fazli</h3>
-            </div>
-            <div className='col-5' style={{color: 'hotpink'}}>
-            <p>Available since: 9/15/21</p>
-            </div>
-        </div>
       </>
     );
 }
