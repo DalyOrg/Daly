f49b7d045d95df3150629fd10f0976a9d005ae6b
Faraz Fazli <farazfazli@gmail.com>
2021-11-21 21:09:22 -0500

    Update quiz results

--- src/webpages/SearchPage.jsx
+++ src/webpages/SearchPage.jsx
@@ -10,20 +10,21 @@ const SearchPage = () => {
     const [results, setResults] = useState([]);
 
     useEffect(() => {
+        setResults([]);
         postSearch(searchTerm).then((res) => {
             res.results.forEach((elem) => {
               if (elem.type === 'quiz') {
                 getQuiz(elem.id).then((quizInfo) => {
                   let tempResults = results;
                   tempResults.push(quizInfo);
-                  setResults(tempResults);
+                  setResults((prevResults) => [...prevResults, quizInfo]);
                   console.log("quizInfo added, results",results);
                 })
               } else {
                 getPlatform(elem.id).then((platformInfo) => {
                   let tempResults = results;
                   tempResults.push(platformInfo);
-                  setResults(tempResults);
+                  setResults((prevResults) => [...prevResults, platformInfo]);
                   console.log("platformInfo added, results",results);
                 });
               }
@@ -38,20 +39,19 @@ const SearchPage = () => {
         <div className='col-4 offset-10'>
         <Funnel style={{color: '#00B5FF', fontSize: '4rem'}}></Funnel>
         </div>
-      <ul>
         {results ? results.map((result) => {
-          <div className='row border-bottom px-3 pb-4 mb-3'>
+          return <div className='row border-bottom px-3 pb-4 mb-3'>
             <div className="col-2 offset-1"><img src="http://placehold.it/200x100"></img></div>
             <div className='col-4'>
-            <h1 style={{color: 'white'}}>{result.name}</h1>
+            <h1 style={{color: 'white'}}>{typeof results.quizzes !== 'undefined' ? "[Platform] " + result.name : "[Quiz] " + result.name}</h1>
             <h3 style={{color: 'hotpink'}}>Created by Faraz Fazli</h3>
             </div>
             <div className='col-5' style={{color: 'hotpink'}}>
-            <p>Available since: 9/15/21</p>
+            <p>{result.timestamp ? "Available since: " + new Date(result.timestamp._seconds*1000).getMonth() + "/" + new Date(result.timestamp._seconds*1000).getDate() + "/" + new Date(result.timestamp._seconds*1000).getFullYear()
+            : ""}</p>
             </div>
         </div>
-        }) : <p>No results found</p>}
-        </ul>
+        }) : <h1 style={{color: 'white'}} className="px-5 pt-2">No Results Found</h1>}
       </div>
       </>
     );
