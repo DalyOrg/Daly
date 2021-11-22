2dc8c9cde614d9023084b0430237c6ae4ecc680f
Faraz Fazli <farazfazli@gmail.com>
2021-11-21 19:38:12 -0500

    Update search page

--- src/webpages/SearchPage.jsx
+++ src/webpages/SearchPage.jsx
@@ -7,26 +7,28 @@ import { useEffect, useState } from "react"
 
 const SearchPage = () => {
     const {searchTerm} = useParams();
-    const [results, setResults] = useState("");
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        let tempResults = []
         postSearch(searchTerm).then((res) => {
             res.results.forEach((elem) => {
               if (elem.type === 'quiz') {
                 getQuiz(elem.id).then((quizInfo) => {
-                  console.log("Quiz Info", quizInfo);
-                  tempResults.push(quizInfo.data);
+                  let tempResults = results;
+                  tempResults.push(quizInfo);
+                  setResults(tempResults);
+                  console.log("quizInfo added, results",results);
                 })
               } else {
                 getPlatform(elem.id).then((platformInfo) => {
-                  console.log("Platform Info", platformInfo);
-                  tempResults.push(platformInfo.data);
+                  let tempResults = results;
+                  tempResults.push(platformInfo);
+                  setResults(tempResults);
+                  console.log("platformInfo added, results",results);
                 });
               }
             })
         })
-        setResults(tempResults);
       }, [])
 
     return (
@@ -36,19 +38,20 @@ const SearchPage = () => {
         <div className='col-4 offset-10'>
         <Funnel style={{color: '#00B5FF', fontSize: '4rem'}}></Funnel>
         </div>
-
+      <ul>
         {results ? results.map((result) => {
           <div className='row border-bottom px-3 pb-4 mb-3'>
             <div className="col-2 offset-1"><img src="http://placehold.it/200x100"></img></div>
             <div className='col-4'>
-            <h1 style={{color: 'white'}}>{result['name']}</h1>
+            <h1 style={{color: 'white'}}>{result.name}</h1>
             <h3 style={{color: 'hotpink'}}>Created by Faraz Fazli</h3>
             </div>
             <div className='col-5' style={{color: 'hotpink'}}>
             <p>Available since: 9/15/21</p>
             </div>
         </div>
-        }) : "No results found"}
+        }) : <p>No results found</p>}
+        </ul>
       </div>
       </>
     );
