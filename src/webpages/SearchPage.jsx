dffff734d9e018ac699a505c881ba15c381bf901
auraBan06 <emre.ban@stonybrook.edu>
2021-12-06 05:51:44 -0500

    fixed order of month and day

--- src/webpages/SearchPage.jsx
+++ src/webpages/SearchPage.jsx
@@ -57,9 +57,10 @@ const SearchPage = () => {
 
             
             <div className="ms-auto p-2 bd-highlight">
-            <p style={{color: 'hotpink'}}>{result.timestamp ? "Available since: " + new Date(result.timestamp).getDate() + "/" 
+            <p style={{color: 'hotpink'}}>{result.timestamp ? "Available since: "
             : ""}
             {new Date(result.timestamp).getMonth() + 1}
+            {"/" +new Date(result.timestamp).getDate()}
             { "/" + new Date(result.timestamp).getFullYear() }
             </p>
             </div>
